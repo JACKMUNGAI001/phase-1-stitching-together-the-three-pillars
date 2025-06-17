d1ae1d00c65728f42ccf76dd3ac0115153171450
Jackson <jacksonmungai001@gmail.com>
2025-06-17 15:31:55 +0300

    new update

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+// ...existing code...
+function superbowlWin(record) {
+  const win = record.find(game => game.result === "W");
+  return win ? win.year : undefined;
+}
+// ...existing code...
